34c042f3fd28eea0b9517b1e647f224243ff2dfe
916-Maria-Popescu <72216642+916-Maria-Popescu@users.noreply.github.com>
2021-12-26 15:12:38 +0200

    set up project structure, draw initial map

---
+++ src/TileMap.js
@@ -0,0 +1,50 @@
+export default class TileMap {
+  constructor(tileSize) {
+    this.tileSize = tileSize;
+    this.yellowDot = new Image();
+    this.yellowDot.src = "../images/yellowDot.png";
+    this.wall = new Image();
+    this.wall.src = "../images/wall.png";
+  }
+  map = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  ];
+  draw(ctx) {
+    console.log("draw");
+
+    for (let row = 0; row < this.map.length; row++) {
+      for (let column = 0; column < this.map[0].length; column++) {
+        let tile = this.map[row][column];
+        if (tile === 1) {
+          this.#drawWall(ctx, column, row);
+        }
+      }
+    }
+  }
+
+  #drawWall(ctx, column, row) {
+    ctx.drawImage(
+      this.wall,
+      column * this.tileSize,
+      row * this.tileSize,
+      this.tileSize,
+      this.tileSize
+    );
+  }
+
+  setCanvasSize(canvas) {
+    canvas.width = this.map[0].length * this.tileSize;
+    canvas.height = this.map.length * this.tileSize;
+  }
+}
